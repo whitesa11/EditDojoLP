c6110c840dbccf7dab461d887ef354d29395665f
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2018-12-18 14:13:47 -0500

    Fixed the whole directory structure

--- static/js/language_selection.js
+++ static/js/language_selection.js
@@ -11,7 +11,7 @@ function generateSelectHTML(learningOrFluent, id, newLanguageList){
     languageListHTML += `<option>${language}</option>`
   });
   let selectHTML = 
-    `<select class="custom-select ed-select" id="${learningOrFluent}${id}">
+    `<select class="custom-select ed-select" name="${learningOrFluent}${id}">
       <option>Select Additional Language (if any)</option>
       ${languageListHTML}
     </select>`;
@@ -43,7 +43,7 @@ function attachSelectionListener(learningOrFluent){
       let selectHTML = generateSelectHTML(learningOrFluent, 2, newLanguageList);
 
       // Also, add a plus button and add a listner to it.
-      $(`#${learningOrFluent}-select`).append(`<img src="static/icons/plus_sign_icon_rainbow.png" alt="Select More Languages" class="ed-plus-sign" style="display: none;">`);
+      $(`#${learningOrFluent}-select`).append(`<img src="/static/icons/plus_sign_icon_rainbow.png" alt="Select More Languages" class="ed-plus-sign" style="display: none;">`);
       $(this).nextAll().fadeIn(100);
       $(this).nextAll().on('click', function(){
         $(this).replaceWith(selectHTML);
